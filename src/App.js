d825def62f621227efdbca641b34668555f2a60e
Ernest Ho <ernest.heh@gmail.com>
2023-04-17 18:33:32 +0800

    render items prop in Accordion component as a list of object

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: "asd12",
       label: "Can I use React on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "jt42",
       label: "Can I use JavaScript on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "hg981",
       label: "Can I use CSS on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
