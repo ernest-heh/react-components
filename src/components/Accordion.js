500253480611387b6ab6549be3da1001df447609
Ernest Ho <ernest.heh@gmail.com>
2023-04-18 14:00:24 +0800

    add logic to display chevron icon as down or left when accordion is open or closed

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  // Define event handler outside mapping function, with index as argument
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        {/* call handleClick as an arrow function, and pass it the index of each item from the map */}
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
