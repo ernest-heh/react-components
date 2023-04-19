70fd1b4f022b1af3ebe0c7a617884d1783692abe
Ernest Ho <ernest.heh@gmail.com>
2023-04-20 00:12:56 +0800

    add logic to allow links to be opened in a new tab

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,13 +5,17 @@ function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    // ALLOW USERS TO CMD OR CTRL CLICK ON A LINK TO OPEN IN A NEW TAB
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
 
   return (
-    <a className="cursor-pointer" onClick={handleClick}>
+    <a className="cursor-pointer" onClick={handleClick} href={to}>
       {children}
     </a>
   );
