8b9872521007cb9ca8f908188a42869f327ef296
Ernest Ho <ernest.heh@gmail.com>
2023-04-20 14:39:00 +0800

    add createPortal to render Modal inside the DOM with body as parent, insert props in Modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,19 @@
-function Modal() {
-  return <div>Modal</div>;
+import ReactDOM from "react-dom";
+
+function Modal({ onClose, children, actionBar }) {
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="absolute inset-0 bg-gray-800 opacity-80"
+      ></div>
+      <div className="absolute inset-20 p-10 bg-white rounded-xl">
+        {children}
+        {actionBar}
+      </div>
+    </div>,
+    document.querySelector(".modal-container")
+  );
 }
 
 export default Modal;
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import Modal from "../components/Modal";
+import Button from "../components/Button";
 
 function ModalPage() {
-  return (
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
     <div>
-      <Modal />
+      <Button primary onClick={handleClose}>
+        Accept
+      </Button>
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is some text for you to agree on and accept.</p>
+    </Modal>
+  );
+
+  return (
+    <div className="relative">
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
+      {showModal && modal}
     </div>
   );
 }
