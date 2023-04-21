32c0cb00fb7080825e8d6dea1dd7e12416bf7111
Ernest Ho <ernest.heh@gmail.com>
2023-04-21 14:27:03 +0800

    add logic to render up & down icons on sortable header columns

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
-  // ACCESS CONFIG FROM PROPS
-  const { config } = props;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
+  // ACCESS CONFIG & DATA FROM PROPS (DESTRUCTURING)
+  const { config, data } = props;
 
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
   };
 
   const updatedConfig = config.map((column) => {
@@ -18,14 +31,54 @@ function SortableTable(props) {
       ...column,
       header: () => (
         <th onClick={() => handleClick(column.label)}>
-          {column.label} IS SORTABLE
+          {getIcons(column.label, sortBy, sortOrder)}
+          {column.label}
         </th>
       ),
     };
   });
 
+  // ONLY SORT DATA IF sortOrder && sortBy !null
+  // MAKE A COPY OF 'data' prop (BECAUSE .sort MUTATES THE ORIGINAL)
+  // FIND THE CORRECT sortValue FUNCTION & USE IT FOR SORTING
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
   // PASS ALL PROPS TO TABLE COMPONENT, OVERRIDE CONFIG WITH updatedConfig
-  return <Table {...props} config={updatedConfig} />;
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} data={sortedData} config={updatedConfig} />
+    </div>
+  );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return "[BOTH]";
+  }
+
+  if (sortOrder === null) {
+    return "[BOTH]";
+  } else if (sortOrder === "asc") {
+    return "[UP]";
+  } else if (sortOrder === "desc") {
+    return "[DOWN]";
+  }
 }
 
 export default SortableTable;
