36278b676749d27d7a03ad4fa3a187130011ea29
Ernest Ho <ernest.heh@gmail.com>
2023-04-21 12:36:32 +0800

    define data and config as props in a Table component, iterate using data & config to render table rows and cells

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,33 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data, config }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((fruit) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(fruit)}
+        </td>
+      );
+    });
+
+    return (
+      // Key can be any unique value
+      <tr className="border-b" key={fruit.name}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
